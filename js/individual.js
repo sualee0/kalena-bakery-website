7339fc67f91beff531e591ca63723b9001062032
Suabcua Lee <sualee@unc.edu>
2024-12-07 15:03:35 -0500

    added all separate pages

--- js/individual.js
+++ js/individual.js
@@ -1,7 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const priceElement = document.querySelector('.price');
     const quantityButtons = document.querySelectorAll('.quantity-controls button');
-    
+    const defaultButton = document.querySelector('.small');
+    if (defaultButton) {
+        defaultButton.classList.add('selected');
+        priceElement.textContent = `$${defaultButton.getAttribute('data-price')}`;
+    } else {
+        console.error('The small button is missing.');
+    }
+
+
     quantityButtons.forEach(button => {
         button.addEventListener('click', () => {
             quantityButtons.forEach(btn => btn.classList.remove('selected'));
